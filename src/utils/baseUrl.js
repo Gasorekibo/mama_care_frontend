6cd6c42d825793d651113963ea79e41bc768f268
Gasorekibo <m.gasore@alustudent.com>
2025-03-08 19:33:20 +0200

    hotfix: add deployed link

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,2 +1,2 @@
-export const baseUrl = " http://localhost:3000/api/v1";
+export const baseUrl = "https://mama-care-backend.onrender.com/api/v1";
 // Working deployed link = https://mama-care-backend.onrender.com
