9e4521b6ec64ef9864bb0a0003f9baa895326809
Gasorekibo <m.gasore@alustudent.com>
2025-03-08 21:49:54 +0200

    fix: tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {
       colors: {
@@ -13,5 +9,5 @@ export default {
       },
     },
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [flowbite.plugin()],
 };
